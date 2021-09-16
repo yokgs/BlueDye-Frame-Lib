40a7d9256ad93101ef54c324d588feb19db2c55f
yokgs <yazid.slila01@gmail.com>
2021-09-16 20:02:12 +0100

    0.0.0

--- raw/bluedye.frame.js
+++ raw/bluedye.frame.js
@@ -1,7 +1,45 @@
-let utilities={
-  mean:function(){},
-  max:function(){},
-  min:function(){},
-  variation:function(){},
+let utilities = {
+  mean: function () { },
+  max: function () { },
+  min: function () { },
+  variation: function () { },
+
+  resize: function (d, s) {
+    let [w, h] = s;
+    h = h || 1;
+    if (d.length !== w * h) {
+      throw new TypeError('');
+    }
+    var r = Array(w);
+    for (let i = 0; i < w; i++) {
+      r[i] = Array(h);
+      for (let j = 0; j < h; j++) {
+        r[i][j] = d[i * h + j];
+      }
+    }
+    return r;
+  },
+  combine: function (d, p) {
+    var k = d.length / p;
+    if (Math.floor(k) !== k) {
+      throw new TypeError('');
+    }
+    var r = Array(k);
+    for (let i = 0; i < k; i++){
+      r[i] = d.slice(i * k, i * k + p);
+    }
+    return r;
+  },
+  safe: function (f) {
+    try {
+      var r = f();
+      return r;
+    } catch (_) { }
+  }
+}
+let frame = function (arr, size) {
+  this.image = utilities.resize(arr, size);
+}
+let effect = function (frame, network) {
   
 }
